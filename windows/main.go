7b1a27f11feaa09a657a99d08f44bb77a418c3b6
felipeweb <felipeweb.programador@gmail.com>
2018-11-08 22:14:19 -0200

    test2

--- windows/main.go
+++ windows/main.go
@@ -19,25 +19,6 @@ func main() {
 		StdIN:     `Hello Windows`,
 	}
 	containerOpts := &provision.ContainerOptions{}
-	project := os.Getenv("GOOGLE_PROJECT")
-	credentials := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
-	if project == "" {
-		log.Fatalln("You must provide a project id from google cloud platform")
-	}
-	if credentials == "" {
-		log.Fatalln("You must provide a path pointing to the credentials file from google cloud platform")
-	}
-	p, err := google.New(
-		project,
-		iaas.WithName("instance-1"),
-		iaas.IsReused(true),
-		iaas.WithRegion("us-east1-b"),
-	)
-	if err != nil {
-		log.Println(err)
-	}
-	buildOpts.Iaas = p
-
 	stdout, _, err := gofn.Run(context.Background(), buildOpts, containerOpts)
 	if err != nil {
 		log.Println(err)
